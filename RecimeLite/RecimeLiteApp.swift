6fab590ee48beebfa315264d412e686f303cd784
Jules Gilos <jules.ian@gmail.com>
2026-03-15 12:26:15 +0800

    Initial Commit

---
+++ RecimeLite/RecimeLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecimeLiteApp.swift
+//  RecimeLite
+//
+//  Created by Jules Ian Gilos on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct RecimeLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
